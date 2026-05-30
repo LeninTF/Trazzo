646c80b7a02f5b4af3c8d590af1915099de18e95
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-05-30 17:28:52 -0500

    vista shop

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/public/index/index';
+import { Shop } from './pages/shop/shop';
 
 export const routes: Routes = [
     {
         path: '',
         component: Index
+    },
+    {
+        path: 'shop',
+        component: Shop
     }
 ];
