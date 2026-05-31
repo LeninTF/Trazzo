0a9fed846f8fe5be98282cf02c5527180dc26cc7
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-05-31 17:36:19 -0500

    vista de contacto

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Index } from './pages/public/index/index';
+import { Form } from './pages/public/form/form';
 import { Shop } from './pages/shop/shop';
 
 export const routes: Routes = [
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'shop',
         component: Shop
+    },
+    {
+        path: 'contacto',
+        component: Form
     }
 ];
