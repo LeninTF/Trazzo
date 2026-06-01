4be79f49eee52e1906950891db4741c7f0b93728
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-06-01 09:25:30 -0500

    vistas legales

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Index } from './pages/public/index/index';
 import { Form } from './pages/public/form/form';
 import { Shop } from './pages/shop/shop';
+import { PrivacyPolicy } from './pages/public/legal/privacy-policy/privacy-policy';
+import { TermsAndConditions } from './pages/public/legal/terms-and-conditions/terms-and-conditions';
 
 export const routes: Routes = [
     {
@@ -15,5 +17,13 @@ export const routes: Routes = [
     {
         path: 'contacto',
         component: Form
+    },
+    {
+        path: 'legal/privacy-policy',
+        component: PrivacyPolicy
+    },
+    {
+        path: 'legal/terms-and-conditions',
+        component: TermsAndConditions
     }
 ];
