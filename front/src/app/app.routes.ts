258813428f127cd283cd6184f7a919773926e685
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-06-03 15:59:43 -0500

    fix(front): correcion para pr

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { Form } from './pages/public/form/form';
 import { Shop } from './pages/shop/shop';
 import { PrivacyPolicy } from './pages/public/legal/privacy-policy/privacy-policy';
 import { TermsAndConditions } from './pages/public/legal/terms-and-conditions/terms-and-conditions';
+import { Login } from './auth/login/login';
+import { Dashboard } from './features/admin-sass/dashboard/dashboard';
+import { Monitoreo } from './features/admin-sass/monitoreo/monitoreo';
+import { Section } from './features/admin-sass/section/section';
 
 export const routes: Routes = [
     {
@@ -13,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'shop',
         component: Shop
+    },
+    {
+        path: 'login',
+        component: Login
     },
     {
         path: 'contacto',
@@ -25,5 +33,50 @@ export const routes: Routes = [
     {
         path: 'legal/terms-and-conditions',
         component: TermsAndConditions
+    },
+    {
+        path: 'admin',
+        pathMatch: 'full',
+        redirectTo: 'admin/dashboard'
+    },
+    {
+        path: 'admin/dashboard',
+        component: Dashboard
+    },
+    {
+        path: 'admin/monitoreo',
+        component: Monitoreo
+    },
+    {
+        path: 'admin/incidencias',
+        component: Section
+    },
+    {
+        path: 'admin/reglas-asistencia',
+        component: Section
+    },
+    {
+        path: 'admin/sedes',
+        component: Section
+    },
+    {
+        path: 'admin/gestion-roles',
+        component: Section
+    },
+    {
+        path: 'admin/configuracion-tenant',
+        component: Section
+    },
+    {
+        path: 'admin/planes',
+        component: Section
+    },
+    {
+        path: 'admin/directorio-personal',
+        component: Section
+    },
+    {
+        path: 'admin/gestion-horarios',
+        component: Section
     }
 ];
