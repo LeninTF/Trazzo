c1d1a01e69cf4a274ad16f38366443f410df2f8f
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-06-03 16:41:35 -0500

    fix(front): Avance vistas de dashboard (#15)
    * fix(front): correcion para pr
    
    * feat(front): creacion vista de dashboard

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly router = inject(Router);
+}
