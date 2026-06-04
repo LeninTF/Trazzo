f73da74d818f77bf5f83bd76d1029c9f0a214096
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-06-04 10:38:04 -0500

    feat(front): vista de incidencias

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { Login } from './auth/login/login';
 import { Dashboard } from './features/admin-tenant/dashboard/dashboard';
 import { Monitoreo } from './features/admin-tenant/monitoreo/monitoreo';
 import { Section } from './features/admin-tenant/section/section';
+import { Incidencias } from './features/admin-tenant/incidencias/incidencias';
 
 export const routes: Routes = [
     {
@@ -49,7 +50,7 @@ export const routes: Routes = [
     },
     {
         path: 'tenant/incidencias',
-        component: Section
+        component: Incidencias
     },
     {
         path: 'tenant/reglas-asistencia',
