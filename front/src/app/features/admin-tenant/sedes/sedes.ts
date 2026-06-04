6b8d344858f16455c664465f175ad5b781c9f405
CarloJP205 <juarezperezcarlos8@gmail.com>
2026-06-04 11:41:25 -0500

    fix(front): creacion de componente sedes

---
+++ front/src/app/features/admin-tenant/sedes/sedes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sedes',
+  imports: [],
+  templateUrl: './sedes.html',
+  styleUrl: './sedes.css',
+})
+export class Sedes {}
