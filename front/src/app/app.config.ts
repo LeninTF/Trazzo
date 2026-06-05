bdacfbb25acf81a10c04fea5d2fc5686a78f4985
CarloJP205 <juarezperezcarlos8@gmail.com>
2026-06-05 15:07:25 -0500

    feat(front): vista dashboard de usuario

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
 
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// Registrar idioma español
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // Configurar locale español
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
   ]
 };
