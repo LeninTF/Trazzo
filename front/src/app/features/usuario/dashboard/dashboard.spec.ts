2d7cb5b64e1c3a7ce71beccc2614d16b434e3d7c
CarloJP205 <juarezperezcarlos8@gmail.com>
2026-06-05 11:35:18 -0500

    feat(front): creación de componentes admin sass

--- front/src/app/features/usuario/dashboard/dashboard.spec.ts
+++ front/src/app/features/usuario/dashboard/dashboard.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Dashboard } from './dashboard';
+import { Tenants } from './tenants';
 
-describe('Dashboard', () => {
-  let component: Dashboard;
-  let fixture: ComponentFixture<Dashboard>;
+describe('Tenants', () => {
+  let component: Tenants;
+  let fixture: ComponentFixture<Tenants>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Dashboard],
+      imports: [Tenants],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Dashboard);
+    fixture = TestBed.createComponent(Tenants);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
