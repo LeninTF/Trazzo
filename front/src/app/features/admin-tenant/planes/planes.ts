304c6734d453cac894acc27b66c63e10d671227a
Josefernando Roberto Alata Bautista <u23272081@utp.edu.pe>
2026-06-18 18:18:16 -0500

    fix(front): correcciones

--- front/src/app/features/admin-tenant/planes/planes.ts
+++ front/src/app/features/admin-tenant/planes/planes.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal, computed, inject } from '@angular/core';
+import { Component, signal, WritableSignal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ToastService } from '../../../services/toast.service';
