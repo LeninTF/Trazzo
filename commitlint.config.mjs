5cdaa2d37ec88758c006e0a2df662e8bc7c1412d
LeninTF <lenin7777.lt@gmail.com>
2026-07-09 01:18:39 -0500

    feat(ci): mas ia (#95)
    Agente AI:
    Se agrego un Agente personalizado
    
    Commitlint:
    Optimización: Se eliminó 'fetch-depth: 0'. Wagoid usa la API de GitHub para los PRs.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,13 +1,21 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    // Tipos de commit permitidos
     'type-enum': [
       2,
       'always',
       ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'ci', 'build']
     ],
-    'subject-case': [0], // importante para dependabot
+    
+    'subject-case': [0], 
+    
+    'body-max-line-length': [0, 'always'],
+    
+    'footer-max-line-length': [0, 'always']
   },
+  
+  //Ignorar commits automáticos de versiones (ej. Dependabot) a nivel de linter
   ignores: [
     (commit) => /^Bump .* from .* to .*$/m.test(commit)
   ]
